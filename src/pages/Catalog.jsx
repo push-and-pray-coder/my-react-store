a25a66147abddfacba70e54bf4d985c9e8916f49
push-and-pray-coder <Titan.Chen@sdgku.edu>
2026-06-28 18:38:52 -0700

    update

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,11 +1,17 @@
 import "./Catalog.css"
 import Product from "../components/Product.jsx"
+import DataService from "../services/dataService.js";
+import { useState } from "react";
 
 function Catalog(){
+
+    const [product, setProduct] = useState ([]);
+
+    
+
     return (
         <div className="product-catalog">
             <h1>Product Catalog</h1>
-            <Product/>
         </div>
     );
 }
